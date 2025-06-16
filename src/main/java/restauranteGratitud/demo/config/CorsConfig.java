2370c4ccd50ab952622dfe7871ccf08c8d1fe418
LuisYepes601 <yepesluis006@gmail.com>
2025-06-15 19:16:33 -0500

    Config

---
+++ src/main/java/restauranteGratitud/demo/config/CorsConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package restauranteGratitud.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CorsConfig {
+      @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("*") // Permite cualquier origen
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
